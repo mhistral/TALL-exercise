7b599b676ec36561c671122fe73324338ba630df
Kimmy <kimberlyparrenas@gmail.com>
2022-04-23 08:47:28 +0800

    Finished Landing Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,9 +15,48 @@
             </div>
         </nav>
 
+        <div class="flex container mx-auto items-center h-full">
+            <div class="flex flex-col w-1/3 items-start">
+
+                <h1 class="text-white font-bold text-5xl leading-tight mb-4">
+                    Simple generic landing page to Subscribe
+                </h1>
+
+                <p class="text-indigo-200 text-xl mb-10">
+                    We Are just checking <span class="font-bold underline">TALL</span> stack. Would you mind subscribing?
+                </p>
+
+                <x-button class="py-3 px-8 bg-red-500 hover:bg-red-600">
+                    Subscribe
+                </x-button>
+
+            </div>
+
+        </div>
+
     </div>
 
     <div class="flex flex-col w-full h-screen bg-pink-500">
+
+        <p class="text-white text-5xl font-extrabold text-center">
+            Let's do it!
+        </p>
+
+        <form class="flex flex-col items-center p-24" action="">
+            <x-input
+            class="px-5 py-3 w-80 border border-blue-400"
+            type="email"
+            name="email"
+            placeholder="Email"></x-input>
+
+            <span class="text-gray-100 text-xs">
+                We will send you a confirmation Email.
+            </span>
+
+            <x-button class="px5 py-3 mt-5 w-80 bg-blue-500 justify-center">
+                Get in
+            </x-button>
+        </form>
 
     </div>
 </x-guest-layout>
