460f91f5babfe63d8254f6bb0cb8fcadcfb46ccc
Kimmy <kimberlyparrenas@gmail.com>
2022-05-31 20:22:57 +0800

    Send Confirmation to Each new Subscriber

--- app/Http/Livewire/LandingPage.php
+++ app/Http/Livewire/LandingPage.php
@@ -4,7 +4,9 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Subscriber;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Auth\Notifications\VerifyEmail;
 
 class LandingPage extends Component
 {
@@ -20,9 +22,14 @@ class LandingPage extends Component
 
     public function subscribe(){
         $this->validate();
-        $subscriber = Subscriber::create([
-            'email' => $this->email,
-        ]);
+        DB::transaction(function () {
+            $subscriber = Subscriber::create([
+                'email' => $this->email,
+            ]);
+
+            $notification = new VerifyEmail;
+            $subscriber->notify($notification); // send notification to the subscriber
+        }, $deadlockRestries = 5); // when the sending verification is in progress and the other one is trying to subscribe, we can retry it serveral times
 
         $this->reset('email');
     }
